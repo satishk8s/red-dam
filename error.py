108eaacaaec615cdd30b3de5ac14dc9bc28f7f70
satishk8s <83827462+satishk8s@users.noreply.github.com>
2024-10-03 18:36:23 +0530

    Update error.py

--- error.py
+++ error.py
@@ -3,40 +3,33 @@ damtest
 
 Response
 {
-  "errorMessage": "An error occurred (AccessDeniedException) when calling the GetItem operation: User: arn:aws:sts::137141637317:assumed-role/red-dam-role-b0q1toew/red-dam is not authorized to perform: dynamodb:GetItem on resource: arn:aws:dynamodb:ap-south-1:137141637317:table/dam_user_filter because no identity-based policy allows the dynamodb:GetItem action",
-  "errorType": "ClientError",
-  "requestId": "2c5dea95-94a9-4205-806d-053574e55168",
+  "errorMessage": "list index out of range",
+  "errorType": "IndexError",
+  "requestId": "26a112ec-8189-4f4a-80eb-dbce5b9d3a66",
   "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 48, in lambda_handler\n    db_users = get_dynamo_db_item(DYNAMODB_TABLE_NAME, key, dynamodb)\n",
-    "  File \"/var/task/lambda_function.py\", line 156, in get_dynamo_db_item\n    response = table.get_item(Key=key)\n",
-    "  File \"/var/lang/lib/python3.12/site-packages/boto3/resources/factory.py\", line 581, in do_action\n    response = action(self, *args, **kwargs)\n",
-    "  File \"/var/lang/lib/python3.12/site-packages/boto3/resources/action.py\", line 88, in __call__\n    response = getattr(parent.meta.client, operation_name)(*args, **params)\n",
-    "  File \"/var/lang/lib/python3.12/site-packages/botocore/client.py\", line 565, in _api_call\n    return self._make_api_call(operation_name, kwargs)\n",
-    "  File \"/var/lang/lib/python3.12/site-packages/botocore/client.py\", line 1021, in _make_api_call\n    raise error_class(parsed_response, operation_name)\n"
+    "  File \"/var/task/lambda_function.py\", line 65, in lambda_handler\n    dam_event = create_event(log_event, database_name, account_id)\n",
+    "  File \"/var/task/lambda_function.py\", line 92, in create_event\n    audit_event = normalize_event(log_event['message'])\n",
+    "  File \"/var/task/lambda_function.py\", line 114, in normalize_event\n    \"username\": audit_event[1],\n"
   ]
 }
 
 Function Logs
-START RequestId: 2c5dea95-94a9-4205-806d-053574e55168 Version: $LATEST
+START RequestId: 26a112ec-8189-4f4a-80eb-dbce5b9d3a66 Version: $LATEST
 event - {'awslogs': {'data': 'eyJsb2dFdmVudHMiOiBbeyJtZXNzYWdlIjogIjEwMXxzYXRpc2ggfCAgY3JlYXRlIHwgfDAgfHwwIHwgODgwIHwgdHlwZSAtIGxvY2F0aW9uLnNwYWNlIHx8IHdyaXRlIHwgZW5kX3RpbWV8fHdoaXRlIDB8fGNoYW5uZWwifV19'}}
 decrypted event - {'logEvents': [{'message': '101|satish |  create | |0 ||0 | 880 | type - location.space || write | end_time||white 0||channel'}]}
 Log Stream: UnknownLogStream
+users in database - {'ResponseMetadata': {'RequestId': '754H9TKE9G1CUBCCRETN3R7MUNVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 03 Oct 2024 13:06:09 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '2', 'connection': 'keep-alive', 'x-amzn-requestid': '754H9TKE9G1CUBCCRETN3R7MUNVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '2745614147'}, 'RetryAttempts': 0}}
+No DynamoDB entry found for database, treating all users as unknown.
 LAMBDA_WARNING: Unhandled exception. The most likely cause is an issue in the function code. However, in rare cases, a Lambda runtime update can cause unexpected function behavior. For functions using managed runtimes, runtime updates can be triggered by a function change, or can be applied automatically. To determine if the runtime has been updated, check the runtime version in the INIT_START log entry. If this error correlates with a change in the runtime version, you may be able to mitigate this error by temporarily rolling back to the previous runtime version. For more information, see https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html
-[ERROR] ClientError: An error occurred (AccessDeniedException) when calling the GetItem operation: User: arn:aws:sts::137141637317:assumed-role/red-dam-role-b0q1toew/red-dam is not authorized to perform: dynamodb:GetItem on resource: arn:aws:dynamodb:ap-south-1:137141637317:table/dam_user_filter because no identity-based policy allows the dynamodb:GetItem action
+[ERROR] IndexError: list index out of range
 Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 48, in lambda_handler
-    db_users = get_dynamo_db_item(DYNAMODB_TABLE_NAME, key, dynamodb)
-  File "/var/task/lambda_function.py", line 156, in get_dynamo_db_item
-    response = table.get_item(Key=key)
-  File "/var/lang/lib/python3.12/site-packages/boto3/resources/factory.py", line 581, in do_action
-    response = action(self, *args, **kwargs)
-  File "/var/lang/lib/python3.12/site-packages/boto3/resources/action.py", line 88, in __call__
-    response = getattr(parent.meta.client, operation_name)(*args, **params)
-  File "/var/lang/lib/python3.12/site-packages/botocore/client.py", line 565, in _api_call
-    return self._make_api_call(operation_name, kwargs)
-  File "/var/lang/lib/python3.12/site-packages/botocore/client.py", line 1021, in _make_api_call
-    raise error_class(parsed_response, operation_name)END RequestId: 2c5dea95-94a9-4205-806d-053574e55168
-REPORT RequestId: 2c5dea95-94a9-4205-806d-053574e55168	Duration: 2679.10 ms	Billed Duration: 2680 ms	Memory Size: 128 MB	Max Memory Used: 88 MB	Init Duration: 617.48 ms
+  File "/var/task/lambda_function.py", line 65, in lambda_handler
+    dam_event = create_event(log_event, database_name, account_id)
+  File "/var/task/lambda_function.py", line 92, in create_event
+    audit_event = normalize_event(log_event['message'])
+  File "/var/task/lambda_function.py", line 114, in normalize_event
+    "username": audit_event[1],END RequestId: 26a112ec-8189-4f4a-80eb-dbce5b9d3a66
+REPORT RequestId: 26a112ec-8189-4f4a-80eb-dbce5b9d3a66	Duration: 1874.20 ms	Billed Duration: 1875 ms	Memory Size: 128 MB	Max Memory Used: 88 MB
 
 Request ID
-2c5dea95-94a9-4205-806d-053574e55168
+26a112ec-8189-4f4a-80eb-dbce5b9d3a66
