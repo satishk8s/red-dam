9f51354a4c73eafd2e73a4082869c0e77fa13f5a
satishk8s <83827462+satishk8s@users.noreply.github.com>
2024-10-08 14:55:32 +0530

    Update error.py

--- error.py
+++ error.py
@@ -3,14 +3,14 @@ dam
 
 Response
 {
-  "error": "Provided data is not base64-encoded"
+  "error": "Failed to parse log events."
 }
 
 Function Logs
-START RequestId: fc771906-eafc-4e0a-b6ac-f62cf9ad03e2 Version: $LATEST
-[ERROR]	2024-10-08T09:17:36.400Z	fc771906-eafc-4e0a-b6ac-f62cf9ad03e2	Provided data is not base64-encoded
-END RequestId: fc771906-eafc-4e0a-b6ac-f62cf9ad03e2
-REPORT RequestId: fc771906-eafc-4e0a-b6ac-f62cf9ad03e2	Duration: 2.04 ms	Billed Duration: 3 ms	Memory Size: 128 MB	Max Memory Used: 81 MB	Init Duration: 536.46 ms
+START RequestId: 7a7ccd7c-d75b-43a3-8331-284368abb702 Version: $LATEST
+[ERROR]	2024-10-08T09:25:14.703Z	7a7ccd7c-d75b-43a3-8331-284368abb702	Error parsing JSON data: Unterminated string starting at: line 1 column 201 (char 200)
+END RequestId: 7a7ccd7c-d75b-43a3-8331-284368abb702
+REPORT RequestId: 7a7ccd7c-d75b-43a3-8331-284368abb702	Duration: 2.29 ms	Billed Duration: 3 ms	Memory Size: 128 MB	Max Memory Used: 81 MB
 
 Request ID
-fc771906-eafc-4e0a-b6ac-f62cf9ad03e2
+7a7ccd7c-d75b-43a3-8331-284368abb702
