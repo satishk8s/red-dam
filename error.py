2ad0e4cfd179964df72a76ad7813bdca133434bc
satishk8s <83827462+satishk8s@users.noreply.github.com>
2024-10-08 14:38:05 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,28 +1,27 @@
 Test Event Name
-damtest
+dam
 
 Response
 {
-  "errorMessage": "'id'",
-  "errorType": "KeyError",
-  "requestId": "6795fe66-02b7-4e64-98b5-1061b6d08b6d",
+  "errorMessage": "Only base64 data is allowed",
+  "errorType": "Error",
+  "requestId": "21379561-8041-48e3-84a6-f6865b9c2723",
   "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 73, in lambda_handler\n    dam_event = create_event(log_event, database_name, account_id)\n",
-    "  File \"/var/task/lambda_function.py\", line 108, in create_event\n    \"messageId\": log_event['id']\n"
+    "  File \"/var/task/lambda_function.py\", line 26, in lambda_handler\n    decoded_data = base64.b64decode(cw_data, validate=True)\n",
+    "  File \"/var/lang/lib/python3.12/base64.py\", line 88, in b64decode\n    return binascii.a2b_base64(s, strict_mode=validate)\n"
   ]
 }
 
 Function Logs
-START RequestId: 6795fe66-02b7-4e64-98b5-1061b6d08b6d Version: $LATEST
-[WARNING]	2024-10-03T13:11:59.748Z	6795fe66-02b7-4e64-98b5-1061b6d08b6d	No DynamoDB entry found for this database, treating all users as unknown.
+START RequestId: 21379561-8041-48e3-84a6-f6865b9c2723 Version: $LATEST
 LAMBDA_WARNING: Unhandled exception. The most likely cause is an issue in the function code. However, in rare cases, a Lambda runtime update can cause unexpected function behavior. For functions using managed runtimes, runtime updates can be triggered by a function change, or can be applied automatically. To determine if the runtime has been updated, check the runtime version in the INIT_START log entry. If this error correlates with a change in the runtime version, you may be able to mitigate this error by temporarily rolling back to the previous runtime version. For more information, see https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html
-[ERROR] KeyError: 'id'
+[ERROR] Error: Only base64 data is allowed
 Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 73, in lambda_handler
-    dam_event = create_event(log_event, database_name, account_id)
-  File "/var/task/lambda_function.py", line 108, in create_event
-    "messageId": log_event['id']END RequestId: 6795fe66-02b7-4e64-98b5-1061b6d08b6d
-REPORT RequestId: 6795fe66-02b7-4e64-98b5-1061b6d08b6d	Duration: 2499.27 ms	Billed Duration: 2500 ms	Memory Size: 128 MB	Max Memory Used: 88 MB	Init Duration: 560.59 ms
+  File "/var/task/lambda_function.py", line 26, in lambda_handler
+    decoded_data = base64.b64decode(cw_data, validate=True)
+  File "/var/lang/lib/python3.12/base64.py", line 88, in b64decode
+    return binascii.a2b_base64(s, strict_mode=validate)END RequestId: 21379561-8041-48e3-84a6-f6865b9c2723
+REPORT RequestId: 21379561-8041-48e3-84a6-f6865b9c2723	Duration: 13.37 ms	Billed Duration: 14 ms	Memory Size: 128 MB	Max Memory Used: 81 MB	Init Duration: 551.05 ms
 
 Request ID
-6795fe66-02b7-4e64-98b5-1061b6d08b6d
+21379561-8041-48e3-84a6-f6865b9c2723
