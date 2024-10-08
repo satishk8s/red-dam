86e98bae5d0715562dfe1caa979fcf8673209385
satishk8s <83827462+satishk8s@users.noreply.github.com>
2024-10-08 15:59:22 +0530

    Update error.py

--- error.py
+++ error.py
@@ -5,10 +5,11 @@ Response
 null
 
 Function Logs
-START RequestId: a90d5543-86ed-4e6a-941d-c34de51e21e6 Version: $LATEST
-[WARNING]	2024-10-08T09:36:49.229Z	a90d5543-86ed-4e6a-941d-c34de51e21e6	Log message format is incorrect, skipping.
-END RequestId: a90d5543-86ed-4e6a-941d-c34de51e21e6
-REPORT RequestId: a90d5543-86ed-4e6a-941d-c34de51e21e6	Duration: 1108.16 ms	Billed Duration: 1109 ms	Memory Size: 128 MB	Max Memory Used: 88 MB
+START RequestId: a0818c5b-a5ae-43ca-822d-acfe95f0dfb8 Version: $LATEST
+[WARNING]	2024-10-08T10:28:50.858Z	a0818c5b-a5ae-43ca-822d-acfe95f0dfb8	No human users found in DynamoDB, treating all users as unknown.
+[WARNING]	2024-10-08T10:28:50.858Z	a0818c5b-a5ae-43ca-822d-acfe95f0dfb8	Log message format is incorrect, skipping.
+END RequestId: a0818c5b-a5ae-43ca-822d-acfe95f0dfb8
+REPORT RequestId: a0818c5b-a5ae-43ca-822d-acfe95f0dfb8	Duration: 1143.59 ms	Billed Duration: 1144 ms	Memory Size: 128 MB	Max Memory Used: 88 MB
 
 Request ID
-a90d5543-86ed-4e6a-941d-c34de51e21e6
+a0818c5b-a5ae-43ca-822d-acfe95f0dfb8
